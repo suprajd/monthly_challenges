5d5785d2129692a7b5f1722c816c96bd9c5dc40c
Supraj Dasari <supraj147@gmail.com>
2023-07-22 15:01:24 +0530

    section 3

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
+#from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january" : "This is January",
@@ -45,8 +45,19 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data)
+        return render(request,  "challenges/challenge.html", {
+            "text": challenge_text,
+            "month_name": month.capitalize()
+        })
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>Thius month is not supported!</h1>")
+    """response_data = render_to_string("challenges/challenge.html")
+    return HttpResponse(response_data)
+    try:
+        challenge_text = monthly_challenges[month]
+        #response_data = f"<h1>{challenge_text}</h1>"
+        response_data = render_to_string("challenges/challenge.html")
+        return HttpResponse(response_data)
+    #except:
+        return HttpResponseNotFound("This month is not supported!")"""
     
